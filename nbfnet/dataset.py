a9cc8f776867f7026cf5d4f19c8b37cf885241ef
zsyJosh <61644849+zsyJosh@users.noreply.github.com>
2022-05-31 23:10:18 -0400

    remove url for FB15k237 ind, add kinship

--- nbfnet/dataset.py
+++ nbfnet/dataset.py
@@ -192,16 +192,16 @@ class FB15k237Inductive(InductiveKnowledgeGraphDataset):
             url = url % version
             save_file = "fb15k237_%s_%s" % (version, os.path.basename(url))
             txt_file = os.path.join(path, save_file)
-            if not os.path.exists(txt_file):
-                txt_file = utils.download(url, self.path, save_file=save_file)
+            assert os.path.exists(txt_file)
+            print("%s exists" % (txt_file))
             train_files.append(txt_file)
         test_files = []
         for url in self.test_urls:
             url = url % version
             save_file = "fb15k237_%s_ind_%s" % (version, os.path.basename(url))
             txt_file = os.path.join(path, save_file)
-            if not os.path.exists(txt_file):
-                txt_file = utils.download(url, self.path, save_file=save_file)
+            assert os.path.exists(txt_file)
+            print("%s exists" % (txt_file))
             test_files.append(txt_file)
 
         self.load_inductive_tsvs(train_files, test_files, verbose=verbose)
@@ -351,4 +351,47 @@ class OGBLBioKG(data.KnowledgeGraphDataset):
             splits.append(split)
             offset += num_sample
             neg_offset += num_sample_with_neg
+        return splits
+
+class kinshipdataset(data.KnowledgeGraphDataset):
+    """
+    Subset of Freebase knowledge base for knowledge graph reasoning.
+
+    Statistics:
+        - #Entity:
+        - #Relation:
+        - #Triplet:
+
+    Parameters:
+        path (str): path to store the dataset
+        verbose (int, optional): output verbose level
+    """
+
+    urls = [
+        "https://github.com/Colinasda/KGdatasets/tree/main/Kinship/train.txt",
+        "https://github.com/Colinasda/KGdatasets/tree/main/Kinship/valid.txt",
+        "https://github.com/Colinasda/KGdatasets/tree/main/Kinship/test.txt",
+    ]
+
+    def __init__(self, path, verbose=1):
+        path = os.path.expanduser(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.path = path
+
+        txt_files = []
+        for url in self.urls:
+            save_file = "kinship_%s" % os.path.basename(url)
+            txt_file = utils.download(url, self.path, save_file=save_file)
+            txt_files.append(txt_file)
+
+        self.load_tsvs(txt_files, verbose=verbose)
+
+    def split(self):
+        offset = 0
+        splits = []
+        for num_sample in self.num_samples:
+            split = torch_data.Subset(self, range(offset, offset + num_sample))
+            splits.append(split)
+            offset += num_sample
         return splits
